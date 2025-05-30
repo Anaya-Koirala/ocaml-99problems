cd04542414aeefcedf4a017f14ebbed9b0f9e654
Anaya Koirala <anaya@koirala.xyz>
2025-05-30 08:43:14 +0545

    format fix

--- 01.ml
+++ 01.ml
@@ -1,8 +1,11 @@
 (* Write a function last : 'a list -> 'a option that returns the last element of a list *)
-let rec last (e : 'a list) : 'a option =
-  match e with [] -> None | [x] -> Some x | _ :: t -> last t
+let rec last (lst : 'a list ): 'a option=
+  match lst with
+  | [] -> None
+  | [x] -> Some x
+  | _ :: tail -> last tail
 ;;
 
 last [1; 2; 3; 4; 5] |> Option.get |> print_int ;;
-
+print_string "\n";;
 last ["1"; "2"; "3"; "4"] |> Option.get |> print_endline
